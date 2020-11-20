8ce4b25e454eb575d777a579403452254cddeea4
Benny Christensen <benny@potatismoose.com>
2020-11-20 10:36:57 +0100

    Helt klar

--- Bankomaten/Program.cs
+++ Bankomaten/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Bankomaten
@@ -8,15 +9,15 @@ namespace Bankomaten
         static Random rand = new Random();
         static double balance = Convert.ToDouble(rand.Next(37, 2274));
         static bool userError = default(bool);
+        static List<double> balancePerYear = new List<double>();
         static void Main(string[] args)
         {
             bool ContinueOn = default(bool);
             bool quit = default(bool);
             string errorMsg = default(string);
             char menuChoice = default(char);
-            
-            
-            
+            double newBalance = default(double);
+            double userMoneyInput = default(double);
             
 
             do
@@ -73,11 +74,11 @@ namespace Bankomaten
                         break;
                     case '2':
                         Console.Write("Ange uttagssumman: ");
-                        double userInput = Convert.ToDouble(Console.ReadLine());
-                        double newBalance = WithDraw(userInput);
+                        userMoneyInput = Convert.ToDouble(Console.ReadLine());
+                        newBalance = WithDraw(userMoneyInput);
                         if (!userError)
                         {
-                            errorMsg = "Du tog ut " + userInput + "kr och har nu kvar " + newBalance + "kr på kontot";
+                            errorMsg = "Du tog ut " + userMoneyInput + "kr och har nu kvar " + newBalance + "kr på kontot";
                             ColorGreen(errorMsg);
                         }
                         else {
@@ -90,11 +91,28 @@ namespace Bankomaten
                         Console.ReadKey();
                         break;
                     case '3':
-                        Console.WriteLine("Val 3");
+                        Console.Write("Ange insättningssumman: ");
+                        userMoneyInput = Convert.ToDouble(Console.ReadLine());
+                        newBalance = Deposit(userMoneyInput);
+                        if (!userError)
+                        {
+                            errorMsg = "satte in " + userMoneyInput + "kr och har nu " + newBalance + "kr på kontot";
+                            ColorGreen(errorMsg);
+                        }
+                        else
+                        {
+                            errorMsg = "Du kan inte sätta in 0 eller ett negativt antal kronor. Saldot är oförändrat.";
+                            ColorRed(errorMsg);
+                            userError = false;
+                        }
+                        errorMsg = default(string);
+                        Console.WriteLine("Tryck valfri tangent för att fortsätta");
                         Console.ReadKey();
                         break;
                     case '4':
-                        Console.WriteLine("Val 4");
+                        Console.Write("Ange antal år du vill hoppa över: ");
+                        int years = Convert.ToInt32(Console.ReadLine());
+                        Intrest(years);
                         Console.ReadKey();
                         break;
                     case 'a':
@@ -151,6 +169,56 @@ namespace Bankomaten
             }
         }
 
+
+        public static double Deposit(double money)
+        {
+            if (money > 0)
+            {
+                balance += money;
+                return balance;
+            }
+            else
+            {
+                userError = true;
+                return balance;
+            }
+        }
+
+        public static void Intrest(int year)
+        {
+            for (int i = 0; i < year; i++)
+            {
+                if (balance <= 1000)
+                {
+                    balance *= 2;
+                    balancePerYear.Add(balance);
+                }
+                else if(balance >1000 && balance <= 5000)
+                {
+                    balance *= 3;
+                    balancePerYear.Add(balance);
+                }
+                else if (balance > 5000 && balance <= 10000)
+                {
+                    balance *= 4;
+                    balancePerYear.Add(balance);
+                }
+                else if (balance > 10000)
+                {
+                    balance *= 5;
+                    balancePerYear.Add(balance);
+                }
+            }
+
+            Console.WriteLine("\nDitt saldo per år:");
+            for (int i = 0; i < balancePerYear.Count; i++)
+            {
+                Console.WriteLine("År " + (i+1) + ": " + balancePerYear[i]);
+            }
+            balancePerYear.Clear();
+
+               
+        }
 
     }
 }
